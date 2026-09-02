88c19cf581edf24fddf06b0553fb7db0a6b48e47
Praneeth3105 <muvvalapraneeth@gmail.com>
2026-09-02 14:10:15 +0530

    Soem changed made

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const connection = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("MongoDB connected");
+    console.log(`MongoDB connected: ${connection.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
